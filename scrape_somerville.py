396f8bcf9bce8bd6e07c8d2d7ff6ba9e618912dd
Kent Johnson <kent3737@gmail.com>
2012-04-26 18:49:05 -0400

    Fix for new web site design

--- scrape_somerville.py
+++ scrape_somerville.py
@@ -5,25 +5,28 @@ import urllib
 import scrapelib
 from lxml import etree
 
-sville_main = 'http://somervillecityma.iqm2.com/Citizens/Default.aspx'
+sville_main = 'http://somervillecityma.iqm2.com/Citizens/Calendar.aspx'
 
 def main():
 	s=scrapelib.Scraper()
-	data = s.urlopen(sville_main)
+	#data = s.urlopen(sville_main)
+	data = open('Calendar.html').read()
 	tree = etree.HTML(data)
 	
-	# The main "Meeting Calendar" table
-	table=tree.xpath("//table[@id='table2']//table[@id='table8']")[0]
-	
-	# The text entries (below the links) are td.MainScreenText
-	# The 'title' attribute of the parent tr contains a lot of info, nicely formatted
-	rows = table.xpath(".//td[@class='MainScreenText']")
+	rows = tree.xpath("//div[@class='RowTop']")	
 	for row in rows:
-		if row.text:
-			attrib = row.getparent().attrib
-			if 'title' in attrib:
-				print attrib['title'].replace('\r', '\n')
-				print
+		anchors = row.xpath('.//a')
+		# First anchor has meeting info in the title attribute
+		print anchors[0].attrib['title'].replace('\r', '\n')
+		
+		# Look for a link to minutes
+		minutes = None
+		for a in anchors:
+			if a.text == 'Minutes':
+				minutes = a.attrib['href']
+		if minutes:
+			print 'Minutes:', minutes
+		print
 			
 if __name__ == '__main__':
 	main()
